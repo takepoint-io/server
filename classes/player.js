9182c76840392f3f796f67f530230fee5d0e29d2
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-05-27 14:18:52 -0500

    populate player with methods

--- classes/player.js
+++ classes/player.js
@@ -1,3 +1,17 @@
 class Player {
-    
+    constructor(id, socket) {
+        this.id = id;
+        this.socket = socket;
+
+        this.spawned = false;
+        this.spawnProt = false;
+        this.health = 0;
+        this.shield = 0
+    }
+
+    sendUpdate(packet) {
+        this.socket.send(packet);
+    }
 }
+
+module.exports = Player;
