57067656d2a03b7508e65477fd43770c0fd269a3
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-05-28 15:42:26 -0500

    add decoding for each packet type

--- classes/packet.js
+++ classes/packet.js
@@ -1,3 +1,5 @@
+const { serverPackets, clientPackets } = require('../data/enums.json');
+
 class Packet {
     constructor(data = {}) {
         this.data = data;
@@ -9,7 +11,7 @@ class Packet {
 
     encode() {
         let packet = this.data;
-        if (packet.type == "ping") return "."
+        if (packet.type == "ping") return serverPackets.ping;
         if (packet.type == "state") {
             return "";
         }
@@ -26,8 +28,65 @@ class Packet {
 
     static decode(packet) {
         let utf8 = new TextDecoder().decode(packet);
+        utf8 = utf8.replace("\x00", "");
         let parts = utf8.split(",");
-        console.log()
+        let opcode = parts[0];
+        let data = {};
+        switch (opcode) {
+            case clientPackets.ping:
+                data.type = "ping";
+                break;
+
+            case clientPackets.chat:
+                data.type = "chat";
+                data.message = parts[1];
+                break;
+
+            case clientPackets.reset:
+                data.type = "reset";
+                break;
+
+            case clientPackets.keypress:
+                data.type = "keypress";
+                data.key = parseInt(parts[1]);
+                data.pressed = parseInt(parts[2]);
+                break;
+
+            case clientPackets.mouse:
+                data.type = "mouse";
+                data.x = parseInt(parts[1]);
+                data.y = parseInt(parts[2]);
+                data.angle = parseInt(parts[3]);
+                break;
+
+            case clientPackets.debug:
+                data.type = "debug";
+                data.value = parts[1];
+                break;
+            
+            case clientPackets.userAgent:
+                data.type = "userAgent";
+                data.value = parts[1];
+                break;
+
+            case clientPackets.spawn:
+                data.type = "spawn";
+                break;
+
+            case clientPackets.gun:
+                data.type = "gun";
+                data.gun = parseInt(parts[1]);
+                break;
+            
+            case clientPackets.adBlock:
+                data.type = "adBlock",
+                data.enabled = parseInt(parts[1]);
+                break;
+
+            default:
+                data.type = "unknown";
+        }
+        return data;
     }
 }
 
