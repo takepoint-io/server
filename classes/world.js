aaeac5381899ff4ebb41696b61bdd05d6384f5b9
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-05-28 15:44:47 -0500

    handle player messages

--- classes/world.js
+++ classes/world.js
@@ -1,15 +1,49 @@
 class World {
-    constructor(radius, points) {
+    inputTypes = ["left", "right", "up", "down", "reload", "space", "mouse"];
+    constructor(radius, points, players) {
         this.radius = radius;
         this.points = points.map((coords, i) => this.coordsToPoint(coords, i));
-        this.players = [];
+        this.players = players;
         this.tickCount = 0;
+    }
+
+    evalTick() {
+        
+    }
+
+    handleMessage(player, data) {
+        switch (data.type) {
+            case "keypress":
+                this.handleKeyInput(player, data.key, data.pressed);
+            case "mouse":
+                this.handleMouseInput(player, data.x, data.y, angle);
+            case "spawn":
+                this.handleSpawn(player);
+        }
+    }
+
+    handleKeyInput(player, key, pressed) {
+        player.inputs[this.inputTypes[key]] = pressed;
+    }
+
+    handleMouseInput(player, x, y, angle) {
+        player.mouse = { x, y, angle };
+    }
+
+    handleSpawn(player) {
+        
+    }
+
+    handlePlayerLeave(player) {
+        
     }
 
     coordsToPoint(point, index) {
         return { 
             x: point[0],
             y: point[1],
+            owner: 0,
+            percentage: 0,
             id: index
         }
     }
