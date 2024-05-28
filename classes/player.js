c1d70e7de8ff8711a134b94f92fa32230c0b7cc3
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-05-28 18:19:20 -0500

    add more player properties

--- classes/player.js
+++ classes/player.js
@@ -2,17 +2,27 @@ class Player {
     constructor(id, socket) {
         this.id = id;
         this.socket = socket;
-
+        
         this.spawnTimeout = 0;
         this.spawned = false;
         this.spawnProt = false;
         this.health = 0;
         this.shield = 0;
+        this.x = 0;
+        this.y = 0;
+        this.spdX = 0;
+        this.spdY = 0;
         this.weapon = {
             id: 0,
             ammo: 0
         };
+        this.teamCode = 0;
+        this.team = null;
         this.registeredEvents = [];
+
+        this.username = "";
+        this.guestName = "";
+        this.loggedIn = false;
 
         this.mouse = {
             x: 0,
@@ -29,6 +39,18 @@ class Player {
             mouse: false
         };
         this.lastInput = Date.now();
+    }
+
+    resetInputs() {
+        this.inputs = {
+            left: false,
+            right: false,
+            up: false,
+            down: false,
+            reload: false,
+            space: false,
+            mouse: false
+        };
     }
 
     sendUpdate(packet) {
