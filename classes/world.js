deff49ba386cd18786eeee705dad9977dd2286bb
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-06-02 00:31:16 -0500

    physics upgrade and bugfixes

--- classes/world.js
+++ classes/world.js
@@ -18,9 +18,7 @@ class World {
         this.bullets = new Map();
         this.tickCount = 0;
         this.tree = new QuadTree(new Box(-4250, -4250, 8500, 8500), { 
-            arePointsEqual: (point1, point2) => { 
-                return point1.data.type == point2.data.type && point1.data.id == point2.data.id;
-            }
+            arePointsEqual: (point1, point2) => point1.data.type == point2.data.type && point1.data.id == point2.data.id
         });
     }
 
@@ -36,49 +34,56 @@ class World {
             player.packet = new Packet();
             player.socket.packetsThisTick = 0;
             for (let event of player.registeredEvents) {
-                if (event == "spawned") {
-                    player.respawn(this);
-                }
-                if (event == "connected") {
-                    player.onConnect(this);
-                    this.removeFromTree(player);
-                }
-                if (event == "disconnected") {
-                    this.players.delete(player.id);
-                    break;
+                switch (event) {
+                    case "spawned":
+                        player.respawn(this);
+                        player.packet.spawn(player);
+                        for (let point of this.points) {
+                            player.packet.pointInfo(point);
+                        }
+                        break;
+                    case "connected":
+                        for (let point of this.points) {
+                            player.packet.pointInfo(point);
+                        }
+                        player.packet.viewbox(player);
+                        break;
                 }
             }
         }
     }
 
     tick() {
+        //updates
         for (let [_playerID, player] of this.players) {
-            if (!player.spawned) continue;
-            player.x += Math.round(player.spdX);
-            player.y += Math.round(player.spdY);
-
-            //Update speed based on player input, 2x acceleration if opposite dir
-            let signX = Math.sign(player.spdX);
-            let signY = Math.sign(player.spdY);
-            if (player.inputs.left  && !player.inputs.right) player.spdX -= worldValues.movement.acceleration * (signX == 1 ? 2 : 1);
-            if (player.inputs.right && !player.inputs.left ) player.spdX += worldValues.movement.acceleration * (signX == 1 ? 1 : 2);
-            if (player.inputs.up    && !player.inputs.down ) player.spdY -= worldValues.movement.acceleration * (signY == 1 ? 2 : 1);
-            if (player.inputs.down  && !player.inputs.up   ) player.spdY += worldValues.movement.acceleration * (signY == 1 ? 1 : 2);
-
-            //Apply a resistive force that increases as velocity increases
-            let resistX = worldValues.movement.dragCoefficient * (player.spdX / player.maxSpeed) + Math.sign(player.spdX) * worldValues.movement.baseResistiveForce;
-            let resistY = worldValues.movement.dragCoefficient * (player.spdY / player.maxSpeed) + Math.sign(player.spdY) * worldValues.movement.baseResistiveForce;
-
-            if (Math.abs(resistX) > Math.abs(player.spdX)) player.spdX = 0;
-            else player.spdX -= resistX;
-            if (Math.abs(resistY) > Math.abs(player.spdY)) player.spdY = 0;
-            else player.spdY -= resistY;
-
-            player.spdX = Util.clamp(player.spdX, -player.maxSpeed, player.maxSpeed);
-            player.spdY = Util.clamp(player.spdY, -player.maxSpeed, player.maxSpeed);
-            player.normalizeSpeed();
+            this.updatePlayerPosition(player);
+            if (player.spawned) {
+                this.updatePlayerVelocity(player);
+            }
         }
         this.updateQuadtree();
+        //collision checking
+
+        //update points
+        for (let point of this.points) {
+            let res = this.queryPlayers(new Circle(point.x, point.y, point.radius + 50))
+                .map(p => this.players.get(p.data.id))
+                .filter(p => p.spawned && Util.hypot(p.x - point.x, p.y - point.y) < point.radius + p.radius);
+            if (res.length) {
+                let statusChanged = point.update(res);
+                if (statusChanged) {
+                    //award the points
+                    if (point.neutralizedThisTick) {
+                        for ()
+                    }
+                    for (let [_playerID, player] of this.players) {
+                        player.packet.pointUpdate(point);
+                        if (point.neutralizedThisTick) player.packet.pointInfo(point);
+                    }
+                }
+            }
+        }
+        
         /*
         vector<int> playersInRange = getObjectsInGrid(playerGrid, gridCollisionLimits);
 
@@ -105,16 +110,38 @@ class World {
 
     postTick() {
         for (let [_playerID, player] of this.players) {
+            if (player.registeredEvents.includes("disconnected")) {
+                this.players.delete(player.id);
+                this.removeFromTree(player);
+                continue;
+            }
             player.packet.playerUpdate(player);
+            if (player.updatedFields.size > 0) {
+                player.packet.playerMiscData(player);
+            }
             player.registeredEvents = [];
+            player.updatedFields.clear();
+        }
+        for (let point of this.points) {
+            point.capturedThisTick = false;
+            point.neutralizedThisTick = false;
         }
     }
 
-    updateQuadtree() {  
-        let tree = this.tree;
-        for (let player in this.players) {
-            tree.insert(new Circle(player.x, player.y, player.radius, { id: player.id, type: player.type }));
+    updateQuadtree() {
+        this.tree.clear();
+        for (let [_playerID, player] of this.players) {
+            if (!player.spawned) continue;
+            this.addCircleToTree(player);
         }
+    }
+
+    queryPlayers(query) {
+        return this.tree.query(query).filter(e => e.data.type == 0);
+    }
+
+    addCircleToTree(obj) {
+        this.tree.insert(new Circle(obj.x, obj.y, obj.radius, { id: obj.id, type: obj.type }));
     }
 
     removeFromTree(obj) {
@@ -126,6 +153,54 @@ class World {
             if (player.packet.data.packetList.length == 0) continue;
             player.sendUpdate(player.packet.enc());
         }
+    }
+
+    collisionCheck() {
+
+    }
+
+    updatePlayerPosition(player) {
+        if (!player.spawned) {
+            //This player is in the lobby, so we just update according to its angle to the center
+            let scrollX = 0;
+            let scrollY = 0;
+            let speed = 2;
+            if (player.loadingScreenDir > 0 && player.loadingScreenDir < 4)  scrollY = -1;
+            if (player.loadingScreenDir > 4) scrollY = 1;
+            if (player.loadingScreenDir == 0 || player.loadingScreenDir == 1 || player.loadingScreenDir == 7) scrollX = -1;
+            if (player.loadingScreenDir > 2 && player.loadingScreenDir < 6 ) scrollX = 1;
+            
+            if (scrollX && scrollY) speed *= Math.SQRT1_2;
+            player.spdX = Math.round(scrollX * speed);
+            player.spdY = Math.round(scrollY * speed);
+
+            if (Math.abs(player.x) > 4000 || Math.abs(player.y) > 4000) player.loadingScreenDir = Math.floor(Util.angle(player.x, player.y) / 45);
+        }
+        player.x += Math.round(player.spdX);
+        player.y += Math.round(player.spdY);
+    }
+
+    updatePlayerVelocity(player) {
+        //Update velocity based on player input, 2x acceleration if opposite dir
+        let signX = Math.sign(player.spdX);
+        let signY = Math.sign(player.spdY);
+        if (player.inputs.left  && !player.inputs.right) player.spdX -= worldValues.movement.acceleration * (signX == 1 ? 2 : 1);
+        if (player.inputs.right && !player.inputs.left ) player.spdX += worldValues.movement.acceleration * (signX == 1 ? 1 : 2);
+        if (player.inputs.up    && !player.inputs.down ) player.spdY -= worldValues.movement.acceleration * (signY == 1 ? 2 : 1);
+        if (player.inputs.down  && !player.inputs.up   ) player.spdY += worldValues.movement.acceleration * (signY == 1 ? 1 : 2);
+
+        //Apply a resistive force that increases as velocity increases
+        let resistX = worldValues.movement.dragCoefficient * (player.spdX / player.maxSpeed) + Math.sign(player.spdX) * worldValues.movement.baseResistiveForce;
+        let resistY = worldValues.movement.dragCoefficient * (player.spdY / player.maxSpeed) + Math.sign(player.spdY) * worldValues.movement.baseResistiveForce;
+
+        if (Math.abs(resistX) > Math.abs(player.spdX)) player.spdX = 0;
+        else player.spdX -= resistX;
+        if (Math.abs(resistY) > Math.abs(player.spdY)) player.spdY = 0;
+        else player.spdY -= resistY;
+
+        player.spdX = Util.clamp(player.spdX, -player.maxSpeed, player.maxSpeed);
+        player.spdY = Util.clamp(player.spdY, -player.maxSpeed, player.maxSpeed);
+        player.normalizeSpeed();
     }
 
     getSpawnPoint(teamCode) {
@@ -166,6 +241,10 @@ class World {
     }
 
     handleKeyInput(player, key, pressed) {
+        if (player.spawned && player.spawnProt) {
+            player.spawnProt = 0;
+            player.updatedFields.set("spawnProt", 0);
+        }
         player.inputs[this.inputTypes[key]] = pressed;
         player.lastInput = Date.now();
     }
@@ -185,6 +264,7 @@ class World {
         player.team = this.getTeam();
         player.teamCode = player.team.id;
         player.team.addPlayer(player);
+        player.loadingScreenDir = Math.floor(Math.random() * 8);
         player.registeredEvents.push("connected");
     }
 
