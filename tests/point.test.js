30edf4e449aa0cb5c555166eec5c25174c3401c8
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-06-04 13:04:18 -0500

    added a new test case

--- tests/point.test.js
+++ tests/point.test.js
@@ -1,4 +1,5 @@
 const Point = require('../classes/point');
+const { worldValues: { points } } = require('../data/values.json');
 const assert = require('assert');
 
 describe('Testing the point class', function() {
@@ -12,5 +13,13 @@ describe('Testing the point class', function() {
         let point = new Point([0, 0], 0);
         point.update([{teamCode: 1}, {teamCode: 1}]);
         assert.equal(point.percentCaptured.toFixed(3), "0.667");
+    });
+
+    it('3. Fully capture a point after the required number of ticks', function() {
+        let point = new Point([0, 0], 0);
+        for (var i = 0; i < points.ticksToCap; i++) {
+            point.update([{teamCode: 1}]);
+        }
+        assert.equal(point.owner, 1);
     });
 });
